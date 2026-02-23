9acc2b3b192be183566f878f0cd998a49c34643d
Luiz451 <luiz.fernando.sf.2006@gmail.com>
2026-02-23 12:20:05 -0300

    testing the Tabuleiro logic

--- test/br/com/cod3r/cm/model/TabuleiroTeste.java
+++ test/br/com/cod3r/cm/model/TabuleiroTeste.java
@@ -1,5 +1,6 @@
 package campo_minado.test.br.com.cod3r.cm.model;
 
+import campo_minado.src.br.com.cod3r.cm.model.Campo;
 import campo_minado.src.br.com.cod3r.cm.model.Tabuleiro;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TabuleiroTeste {
 
-    private Tabuleiro tabuleiro;
+    @Test
+    void testeObjetivoAlcancadoTodosTrue() {
+        Tabuleiro tabuleiro = new Tabuleiro(2, 2, 0);
 
-    @BeforeEach
-    void iniciarTabuleiro() {
-        tabuleiro = new Tabuleiro(5, 5, 7);
+        tabuleiro.abrir(1, 1);
+        tabuleiro.abrir(1, 2);
+        tabuleiro.abrir(2, 1);
+        tabuleiro.abrir(2, 2);
+
+        assertTrue(tabuleiro.objetivoAlcancado(), "O tabuleiro novo deve constar como resolvido");
+    }
+
+    @Test
+    void testeObjetivoAlcancadoFalse() {
+        Tabuleiro tabuleiro = new Tabuleiro(2, 2, 0);
+
+        assertFalse(tabuleiro.objetivoAlcancado(), "O tabuleiro novo não deve constar como resolvido");
     }
 }
