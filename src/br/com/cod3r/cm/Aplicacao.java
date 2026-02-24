6c597033eac5a3e406c4507f287d078c1900de50
Luiz451 <luiz.fernando.sf.2006@gmail.com>
2026-02-24 11:23:30 -0300

    The final main method

--- src/br/com/cod3r/cm/Aplicacao.java
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -1,16 +1,12 @@
 package campo_minado.src.br.com.cod3r.cm;
 
 import campo_minado.src.br.com.cod3r.cm.model.Tabuleiro;
+import campo_minado.src.br.com.cod3r.cm.vision.TabuleiroConsole;
 
 public class Aplicacao {
     public static void main(String[] args) {
 
         Tabuleiro tabuleiro = new Tabuleiro(10, 10, 6);
-
-        tabuleiro.alternarMarcacao(4, 4);
-        tabuleiro.alternarMarcacao(4, 5);
-        tabuleiro.abrir(3, 3);
-
-        System.out.println(tabuleiro);
+        new TabuleiroConsole(tabuleiro);
     }
 }
