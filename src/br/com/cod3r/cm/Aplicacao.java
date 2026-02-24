df164ee378925476e9ccbbc5fd20851b9f3647d8
Luiz451 <luiz.fernando.sf.2006@gmail.com>
2026-02-24 10:11:44 -0300

    Creating creating the class with the main method

--- src/br/com/cod3r/cm/Aplicacao.java
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -1,4 +1,16 @@
 package campo_minado.src.br.com.cod3r.cm;
 
+import campo_minado.src.br.com.cod3r.cm.model.Tabuleiro;
+
 public class Aplicacao {
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(10, 10, 6);
+
+        tabuleiro.alternarMarcacao(4, 4);
+        tabuleiro.alternarMarcacao(4, 5);
+        tabuleiro.abrir(3, 3);
+
+        System.out.println(tabuleiro);
+    }
 }
